141eda202682a6b094b03bfd26d91df840e4e588
unknown <aldmyatyfrhh@gmail.com>
2026-03-29 21:44:08 +0200

    fix Bugs of sending email

--- src/jobs/job.module.ts
+++ src/jobs/job.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserCleanupJob } from './user-cleanup.job';
 import { DataBaseModule } from 'src/modules/db/database.module';
+import { OutboxJob } from './outbox.job';
+import { MailModule } from 'src/shared/mail/mail.module';
 
 @Module({
-  providers: [UserCleanupJob],
-  imports: [DataBaseModule],
+  providers: [UserCleanupJob, OutboxJob],
+  imports: [DataBaseModule, MailModule],
 })
 export class JobModule {}
---
+++ src/jobs/outbox.job.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { EVENT_TYPE, STATUS_OUTBOX } from 'generated/prisma/enums';
+import { DataBaseService } from 'src/modules/db/database.service';
+import { MailService } from 'src/shared/mail/mail.service';
+
+@Injectable()
+export class OutboxJob {
+  constructor(
+    private prisma: DataBaseService,
+    private config: ConfigService,
+    private mailService: MailService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async processOutbox() {
+    const now = new Date();
+
+    const messages = await this.prisma.outbox.findMany({
+      where: {
+        status: STATUS_OUTBOX.PENDING,
+        nextRetryAt: { lte: now },
+      },
+      take: 10,
+      orderBy: {
+        created_at: 'asc',
+      },
+    });
+
+    for (const msg of messages) {
+      const { email, token } = msg.payload as {
+        email: string;
+        token: string;
+      };
+      try {
+        if (msg.event_type === EVENT_TYPE.SEND_VERIFICATION_EMAIL) {
+          const link = `${this.config.get<string>('DOMAIN')}/api/v1/auth/verify-email?token=${token}`;
+
+          await this.mailService.sendVerifyEmail(email, link);
+        }
+
+        if (msg.event_type === EVENT_TYPE.SEND_RESET_PASSWORD) {
+          const link = `${this.config.get<string>('DOMAIN')}/api/v1/auth/password/reset?token=${token}`;
+
+          await this.mailService.resetPassword(email, link);
+        }
+
+        await this.prisma.outbox.update({
+          where: { id: msg.id },
+          data: {
+            status: STATUS_OUTBOX.SENT,
+          },
+        });
+      } catch (err) {
+        console.log(err);
+        const newRetryCount = Number(msg.retryCount + 1);
+        const delay = this.getDelay(newRetryCount);
+
+        const newStatus =
+          newRetryCount > 3 ? STATUS_OUTBOX.FAILED : STATUS_OUTBOX.PENDING;
+
+        await this.prisma.outbox.update({
+          where: { id: msg.id },
+          data: {
+            status: newStatus,
+            retryCount: newRetryCount,
+            nextRetryAt: new Date(Date.now() + delay),
+          },
+        });
+      }
+    }
+  }
+
+  private getDelay(retryCount: number): number {
+    if (retryCount === 1) return 60 * 1000;
+    if (retryCount === 2) return 5 * 60 * 1000;
+    if (retryCount === 3) return 15 * 60 * 1000;
+
+    return 60 * 60 * 1000;
+  }
+}
